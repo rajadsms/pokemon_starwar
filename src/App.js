5cddd7f7700306819c53c0624de970a11470bed2
Raja Dey <raja.d@fnp.com>
2023-08-16 02:13:19 +0530

    puhed bug fix

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import { useState } from 'react';
 function App() {
   const [selection,setSelection]=useState("pokemon")
   const onChangeValue=(evt)=>{
-  setSelection(evt.target.value)  
+  setSelection(evt?.target?.value||"pokemon")  
   }
   return (
     <div className="App">
-      <div onChange={onChangeValue}>
+      <div onChange={onChangeValue} className='checkBox'>
         <input type="radio" value="pokemon" name="assignment" checked={selection==="pokemon"}/> Pokemon
         <input type="radio" value="starwar" name="assignment" checked={selection!=="pokemon"}/> Starwar
         </div>
